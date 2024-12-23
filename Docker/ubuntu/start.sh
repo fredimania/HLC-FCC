fce896d2f5d4e91427d1b3e97bf98833508a6f57
fredimania <fredimania@gmail.com>
2024-12-23 22:15:26 +0100

    volumen y variable ent.log

--- Docker/ubuntu/start.sh
+++ Docker/ubuntu/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# carga las variables d entorno
 set -e
-echo "COnfigura mis ervidorr " > /root/f.log
+
+echo "COnfigura mis Servidorr " >> /root/datos/archivo.log
+echo "${USUARIO}es el usuario " >> /root/datos/archivo.log
+echo "${PROCESO}es el proceso " >> /root/datos/archivo.log
+
+#buscaqueda de fichero
+if grep -q "${USUARIO}" "/etc/passwd"; then
+    echo "${USUARIO} se encuentra en el sistemaa" >> /root/datos/archivo.log
+else
+    echo "${USUARIO} no se encuentra en el sistema " >> /root/datos/archivo.log
+fi
+
 tail -f /dev/null
